29a3f45382a2619fa9e60e5224965b08c6f6bb5b
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-07-27 10:04:06 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using static Factors;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("4: " + CountFactorsToString(4));
+Console.WriteLine("8: " + CountFactorsToString(8));
+Console.WriteLine("12: " + CountFactorsToString(12));
+
+long number = 4444488889;
+Console.WriteLine(number + ": " + CountFactorsToString(number));
